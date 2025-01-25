4984d713905e254a8a07294c752e9ca34a38caf4
karthicksanthosh <karthick.santhosh1999@gmail.com>
2025-01-25 20:37:10 +0530

    Wokring on product create function

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,4 +1,5 @@
 const { imageUploadUtils } = require("../../config/cloudnary");
+const Product = require("../../models/Product");
 const CustomeError = require("../../utils/customeErrorHandler");
 
 const handleImageUpload = async (req, res, next) => {
@@ -20,4 +21,104 @@ const handleImageUpload = async (req, res, next) => {
   }
 };
 
-module.exports = { handleImageUpload };
+const addProduct = async (req, res, next) => {
+  const {
+    image,
+    title,
+    description,
+    category,
+    brand,
+    price,
+    salePrice,
+    totalStack,
+  } = req.body;
+  try {
+    const newCreatedProduct = new Product({
+      image,
+      title,
+      description,
+      category,
+      brand,
+      price,
+      salePrice,
+      totalStack,
+    });
+    res.status(201).json({
+      message: "Product created successfully",
+      success: true,
+      statusCode: 201,
+      data: newCreatedProduct,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const getAllProducts = async (req, res, next) => {
+  try {
+    const allProducts = await Product.find({});
+  } catch (error) {
+    next(error);
+  }
+};
+
+const getSingleProduct = async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const product = await Product.findById({
+      id,
+    });
+    if (!product) throw CustomeError("Product not exist", 400);
+    res.status(200).json({
+      message: "Product get successfully",
+      statusCode: 200,
+      success: true,
+      data: product,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const deleteSingleProduct = async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const product = await Product.findById(id);
+    if (!product) throw CustomeError("Product not exist", 400);
+    await Product.findByIdAndDelete(id);
+    res.status(200).json({
+      message: "Product deleted successfully",
+      statusCode: 200,
+      success: true,
+      data: product,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const updateProduct = async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const product = await Product.findById(id);
+    if (!product) throw CustomeError("Product not available", 400);
+    await Product.findByIdAndUpdate(id, req.body, { new: true });
+    res.status(200).json({
+      message: "Product updated successfully",
+      success: true,
+      statusCode: 200,
+      data: product,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = {
+  handleImageUpload,
+  addProduct,
+  getAllProducts,
+  getSingleProduct,
+  deleteSingleProduct,
+  updateProduct,
+};
--- routes/admin-routes/admin.routes.js
+++ routes/admin-routes/admin.routes.js
@@ -2,11 +2,21 @@ const express = require("express");
 
 const {
   handleImageUpload,
+  addProduct,
+  deleteSingleProduct,
+  getAllProducts,
+  getSingleProduct,
+  updateProduct,
 } = require("../../controllers/admin/product.controller");
 const { upload } = require("../../config/cloudnary");
 
 const adminRoute = express.Router();
 
 adminRoute.post("/upload-image", upload.single("my_file"), handleImageUpload);
+adminRoute.post("/add-product", addProduct);
+adminRoute.get("/get-all-products", getAllProducts);
+adminRoute.get("/get-single-product/:id", getSingleProduct);
+adminRoute.delete("/delete-product", deleteSingleProduct);
+adminRoute.put("/update-product/:id", updateProduct);
 
 module.exports = adminRoute;
